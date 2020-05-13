388798aad23e414a0b7f046bc67929e19651d082
Ty Mondragon <tymondragon@gmail.com>
2020-05-12 19:03:16 -0600

    Alter method to print lucky if an integer has '3'

--- src/test/java/com/fizzbuzz/AppTest.java
+++ src/test/java/com/fizzbuzz/AppTest.java
@@ -16,34 +16,39 @@ public class AppTest
         assertTrue( true );
     }
 
+//    @Test
+//    public void shouldReturnFizz() {
+//        assertEquals("Returns fizz", fizzBuzz.fizzOrBuzzOrLucky(3), "fizz");
+//    }
+
     @Test
-    public void shouldReturnFizz() {
-        assertEquals("Returns fizz", fizzBuzz.fizzOrBuzz(3), "fizz");
+    public void shouldReturnLucky() {
+        assertEquals("prints lucky if integer contains a 3", fizzBuzz.fizzOrBuzzOrLucky(13), "lucky");
     }
 
     @Test
     public void shouldReturnBuzz() {
-        assertEquals("Returns buzz", fizzBuzz.fizzOrBuzz(5), "buzz");
+        assertEquals("Returns buzz", fizzBuzz.fizzOrBuzzOrLucky(5), "buzz");
     }
 
     @Test
     public void shouldReturnFizzbuzz() {
-        assertEquals("Returns fizzbuzz", fizzBuzz.fizzOrBuzz(15), "fizzbuzz");
+        assertEquals("Returns fizzbuzz", fizzBuzz.fizzOrBuzzOrLucky(15), "fizzbuzz");
     }
 
     @Test
     public void shouldReturnNumber() {
-        assertEquals("Returns integer inserted when it is not divisible by 3 or 5", fizzBuzz.fizzOrBuzz(4), "4");
+        assertEquals("Returns integer inserted when it is not divisible by 3 or 5", fizzBuzz.fizzOrBuzzOrLucky(4), "4");
     }
 
     //stringBuilder
     @Test
     public void shouldReturnAStringOfFizzbuzz() {
-        assertEquals("Returns a concatenated string result", fizzBuzz.buildString(5), "1 2 fizz 4 buzz");
+        assertEquals("Returns a concatenated string result", fizzBuzz.buildString(5), "1 2 lucky 4 buzz");
     }
 
     @Test
     public void trimsWhiteSpace() {
-        assertNotEquals("String with tailing whitespace", fizzBuzz.buildString(5), "1 2 fizz 4 buzz ");
+        assertNotEquals("String with tailing whitespace", fizzBuzz.buildString(5), "1 2 lucky 4 buzz ");
     }
 }
